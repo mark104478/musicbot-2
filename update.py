0c0a383bb4dcaef0f4f2e1b20e226b9523804bbf
Joseph Banks <josephbanks7@gmail.com>
2016-09-16 20:58:59 +0100

    Create update.py

---
+++ update.py
@@ -0,0 +1,10 @@
+import subprocess
+
+print("Welcome to MusicBot update tool")
+print("Preparing to download MusicBot")
+print("Ignore stuff below this line unless you are debugging")
+print("="*50)
+print("RUNNING UPDATE SCRIPT")
+print(subprocess.check_output("git pull",shell=True).decode()
+print("="*50)
+print("Update complete")
