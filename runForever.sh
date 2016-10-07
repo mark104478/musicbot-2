9945f5d2511ed3c50b81546de543a04b760cc021
Joseph Banks <josephbanks7@gmail.com>
2016-10-07 17:43:32 +0100

    Added help with the *.sh scripts

--- runForever.sh
+++ runForever.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # If you have issues running this try this code
 # sudo chmod 777 runForever.sh
+echo If you get any issues running this script then read the help in the file
 while :
 do
 	node bot.js
